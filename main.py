36b8be3d0b8658087116aaa8aa0a9a41ea6b40fa
Heidi Yao Jiang <hyjiang@Laptop-2.local>
2025-02-15 18:42:33 -0500

    changes

--- main.py
+++ main.py
@@ -5,14 +5,9 @@ FILENAME = "ma_suffolk.csv"
 def read_csv(filename, skip = 1):
     data = []
     with open(filename, "r") as infile:
-        csvfile = csv.reader(infile)
-        next(csvfile)
+        data = []
+        csvfile = csv.DictReader(filename)
         for row in csvfile:
-            key = row[0]
-            values = [val for val in row]
-            data[key] = values
-    return data
+            data.append(row)
+        return data
 
-data = read_csv(FILENAME)
-for row in data:
-    print(row)
