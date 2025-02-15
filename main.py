7e9044ca603e8550340c25df76ba5c4149ae47bc
Heidi Yao Jiang <hyjiang@Laptop-2.local>
2025-02-15 18:21:46 -0500

    c

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 
-FILENAME = "housing.csv"
+FILENAME = "ma_suffolk.csv"
 
 def read_csv(filename, skip = 1):
     data = []
@@ -12,13 +12,5 @@ def read_csv(filename, skip = 1):
             data.append(row)
     return data
 
-import pandas as pd
-
-# Example: Retrieve property tax parcels data
-url = "https://maps.massgis.digital.mass.gov/server/rest/services/Property_Tax_Parcels/FeatureServer/0/query?where=1%3D1&outFields=*&f=geojson"
-
-# Read the JSON and convert to CSV
-df = pd.read_json(url)
-df.to_csv("property_tax_parcels.csv", index=False)
-
-print(df)
+data = read_csv(FILENAME)
+print(data)
