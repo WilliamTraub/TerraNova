4a9297ee70c9c7b037ca9d1950946a3b0913d2ba
Heidi Yao Jiang <hyjiang@Laptop-2.local>
2025-02-15 18:38:04 -0500

    change

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ def read_csv(filename, skip = 1):
     data = []
     with open(filename, "r") as infile:
         csvfile = csv.reader(infile)
-        for _ in range(skip):
-            next(csvfile)
+        next(csvfile)
         for row in csvfile:
-            data.append(row)
+            key = row[0]
+            values = [val for val in row]
+            data[key] = values
     return data
 
 data = read_csv(FILENAME)
-print(data)
+for row in data:
+    print(row)
