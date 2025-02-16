0c5c15b7232c6bfc8a3744b930d6ae858c4a5d2d
Heidi Yao Jiang <hyjiang@Laptop-2.local>
2025-02-15 19:30:32 -0500

    updates

--- land.py
+++ land.py
@@ -1,12 +1,24 @@
 class Land:
-    def __init__(self, lat, lon, land_value):
+    def __init__(self, lat, lon, land_value = 0):
         self.lat = lat
-        self.land_value = land_value
-        self.pollution = []
         self.lon = lon
+        self.land_value = land_value
 
-    def add_pollution(self, var):
-        self.pollution.append(var)
+    def __str__(self):
+        return f"Land at {self.lat}, {self.lon} with value {self.land_value}"
+    
+    def add_haversine(self, other):
+        lat1, lat2 = math.radians(self.lat), math.radians(other.lat)
+        long1, long2 = math.radians(self.long), math.radians(other.long)
+
+        lat_delta = lat2 - lat1
+        long_delta = long2 - long1
+
+        a = (math.sin(lat_delta / 2)) ** 2 + math.cos(lat1) * math.cos(lat2) * \
+        (math.sin(long_delta / 2)) ** 2
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+        self.haversine = c * r
 
     def dist(self, other):
         return ((self.lat - other.lat) ** 2 + (self.lon - other.lon) ** 2) ** 0.5
@@ -14,7 +26,4 @@ class Land:
     def calculate_suitability_score(self):
         score = 100 - (self.pollution * 0.5) - (self.crime_rate * 0.3) + (self.green_space * 0.7) + ((100 - self.traffic) * 0.5)
         return max(0, min(score, 100))
-    
-    def __str__(self):
-        return f"Land at {self.lat}, {self.lon} with value {self.land_value}"
     
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import csv
 from land import Land
 
 FILENAME = "ma_suffolk.csv"
-CITYCENTER = (42.3394, -71.0940)
+CITYCENTER = Land(42.3394, -71.0940)
 
 def read_csv(filename):
     data = []
@@ -21,24 +21,23 @@ def float_2d(lst):
         for i in range(len(row)):
             row[i] = float(row[i])
 
-def generate_tuples(lst1, lst2):
-    tuples = []
-    for i in range(len(lst1)):
-        tuples.append((lst1[i], lst2[i]))
-    return tuples
-
 def lands(lat, long, land_val):
+    land_lst = []
     for i in range(len(lat)):
         land = Land(lat[i], long[i], land_val[i])
-
+        land_lst.append(land)
+    return land_lst
 
 def main():
     data = read_csv(FILENAME)
     lat_vals = get_val(data, "lat")
     long_vals = get_val(data, "lon")
     land_vals = get_val(data, "landval")
+
     float_2d([lat_vals, long_vals, land_vals])
-    lat_long = generate_tuples(lat_vals, long_vals)
-    print(lat_vals)
+
+    land_lst = lands(lat_vals, long_vals, land_vals)
+    for land in land_lst:
+        print(land)
 
 main()
